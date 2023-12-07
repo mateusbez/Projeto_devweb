7bc77ef42f31ad07d1e5e9b63c43d4c8e003b921
Mateus <mateus.sbezerra@dataprev.gov.br>
2023-12-07 09:38:20 -0300

    correções

--- src/main/java/com/example/demo/controller/ClienteController.java
+++ src/main/java/com/example/demo/controller/ClienteController.java
@@ -1,4 +1,4 @@
-package com.example.demo.controllers;
+package com.example.demo.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.annotation.Secured;
