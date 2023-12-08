8f77ed3db1d08686366219e43ef692c23aa41154
Mateus <mateus.sbezerra@dataprev.gov.br>
2023-12-08 10:10:08 -0300

    Correções

--- src/main/java/com/example/demo/repositories/EstoqueRepository.java
+++ src/main/java/com/example/demo/repositories/EstoqueRepository.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.demo.entities.Estoque;
+import com.example.demo.entities.Cliente;
 
 public interface EstoqueRepository extends JpaRepository<Estoque, String> {
     List<Estoque> findByCliente(Cliente cliente);
